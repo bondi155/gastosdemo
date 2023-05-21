2f415bfc3b13ac2796867bceb3d71af4f9b8c394
bondi155 <78815412+bondi155@users.noreply.github.com>
2023-05-21 11:29:07 -0600

    spinner commit

--- backend/controllers/ChatCompletions.js
+++ backend/controllers/ChatCompletions.js
@@ -2,32 +2,116 @@ const { Configuration, OpenAIApi } = require('openai');
 const configuration = new Configuration({
   apiKey: process.env.OPENAI_API_KEY,
 });
+const axios = require('axios');
 
 const openai = new OpenAIApi(configuration);
 
 async function ChatCompletions(req, res) {
   const prompt = req.body.prompt;
+  const messages = req.body.messages;
+
+  const gastosKeywords = [
+    'mis ultimos gastos',
+    'mis datos de gasto',
+    'mi tabla de gastos',
+    'mi informacion de gastos',
+  ];
+  const result = gastosKeywords.some((gastosKeywords) =>
+    prompt.includes(gastosKeywords)
+  );
+  let responseGastos = [];
 
   if (!prompt) {
     console.error('No se proporcionó ninguna pregunta.');
-    return res.status(400).json({ error: 'No se proporcionó ninguna pregunta.' });
+    return res
+      .status(400)
+      .json({ error: 'No se proporcionó ninguna pregunta.' });
   }
-  try {
-    const response = await openai.createChatCompletion({
-      model: 'gpt-3.5-turbo',
-      messages: [
-        { role: 'system', content: 'You are a helpful assistant.' },
-        { role: 'user', content: prompt },
-      ],
-    });
-    res.json({ message: response.data.choices[0].message.content.trim() });
-    console.log('la respuesta de chat es :', response.data.choices[0].message);
-  } catch (error) {
-    res.status(500).json({ error: error.toString() });
-    console.error('Error al llamar a la API de OpenAI:', error.code, error.response.status, error.response.data);
-    //    console.error('Error al llamar a la API de OpenAI:', error.code, error.response.status, error.response.data);
 
+  if (result) {
+    try {
+      const apiResponse = await axios.get(
+        'http://localhost:5005/consultaformgasto'
+      );
+      responseGastos = apiResponse.data;
+
+      const gastosMessage = `El usuario tiene los siguientes datos de gastos:
+        - Formato de pago: deposito
+        - FTO: 12345
+        - Sector: contadoria
+        - Razón Social: proveedor 1
+        - Folio: 123456
+        - Total: 5567
+        - descripcion : silla
+        `;
+      //console.log(responseGastos); falta crear de resppnseGastsos la respuesta en lenguaje natural acordarse
+
+      const newPrompt = `${prompt}\n${gastosMessage}`;
+
+      console.log(gastosMessage);
+
+      const responseGpt = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: newPrompt },
+        ],
+      });
+
+      const finalResponse = {
+        message: responseGpt.data.choices[0].message.content.trim(),
+        responseGastos: responseGastos,
+      };
+
+      res.json(finalResponse);
+      console.log(
+        'la respuesta de chat es :',
+        responseGpt.data.choices[0].message
+      );
+    } catch (error) {
+      console.error(
+        'Error en la solicitud de la API interna de gastos:',
+        error
+      );
+    }
+  } else {
+    //llamada normal a api , hay que factorizar esto luego
+    try {
+      const response = await openai.createChatCompletion({
+        model: 'gpt-3.5-turbo',
+        messages: [
+          ...messages.map((message) => ({
+            role: message.sender === 'Usuario' ? 'user' : 'assistant',
+            content: message.text,
+          })),
+          { role: 'system', content: 'You are a helpful assistant.' },
+          { role: 'user', content: prompt },
+          
+        ],
+      });
+      res.json({ message: response.data.choices[0].message.content.trim() });
+      console.log(
+        'la respuesta de chat es :',
+        response.data.choices[0].message
+      );
+    } catch (error) {
+      res.status(500).json({ error: error.toString() });
+      console.error(
+        'Error al llamar a la API de OpenAI:',
+        error.code,
+        error.response.status,
+        error.response.data
+      );
+      //    console.error('Error al llamar a la API de OpenAI:', error.code, error.response.status, error.response.data);
+    }
   }
+
+  //catch (error) {
+  //res.status(500).json({ error: error.toString() });
+  //console.error('Error al llamar a la API de OpenAI:', error.code, error.response.status, error.response.data);
+  //console.error('Error al llamar a la API de OpenAI:', error.code, error.response.status, error.response.data);
+
+  //}
 }
 
 module.exports = {
--- src/pages/OpenIA.js
+++ src/pages/OpenIA.js
@@ -1,55 +1,82 @@
-import React,{useState} from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
 import { Form } from 'react-bootstrap';
-import {
-  Container,
-  Button,
-} from 'react-bootstrap';
+import { Container, Button, Spinner } from 'react-bootstrap';
 
+function OpenIA() {
+  const [messages, setMessages] = useState([]);
+  const [prompt, setPrompt] = useState('');
+  const [loading, setLoading] = useState(false);
 
-function OpenIA () {
+  const handleClick = () => {
+    setLoading(true);
+    // Agregar el mensaje del usuario al array de mensajes anteriormente enviado +
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { text: prompt, sender: 'Usuario' },
+    ]);
+    axios
+ .post('http://localhost:5005/chatcompl', {
+        prompt,
+        messages
+      })
+      .then((response) => {
+        // Agregar el mensaje de la IA al array de mensajes (mensaje que manda chatgpt mapeado)
+        setMessages((prevMessages) => [
+          ...prevMessages,
+          { text: response.data.message, sender: 'IA' },
+        ]);
+        setLoading(false);
+      })
+      .catch((error) => {
+        console.log(error);
+        setLoading(false);
+      });
 
-    const [iares, setIares] = useState ({})
+    // Limpiar el prompt
+    setPrompt('');
+  };
+  return (
+    <>
+      <Container className='mt-5'>
+        <h1>Conversación con IA</h1>
+        <Form.Group className='mb-3 mt-3'>
+          <Form.Control
+            as='textarea'
+            required
+            name='prompt'
+            placeholder='ingresa pregunta'
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
+          />
+        </Form.Group>
+        {loading ? (
+          <Button variant='primary' disabled>
+            <Spinner
+              as='span'
+              animation='grow'
+              size='sm'
+              role='status'
+              aria-hidden='true'
+            />
+            Loading...
+          </Button>
+        ) : (
+          <Button className='' onClick={handleClick}>
+            Preguntar
+          </Button>
+        )}
 
-
-    const [prompt, SetPrompt] = useState ('')
-
-
-    const handleclick = () => { 
-    axios.post("http://localhost:5005/chatcompl", {
-     prompt: prompt //aca poner usuario cuando se defina : usuario: usuario,
-    })
-    .then((response)  => {
-     setIares(response.data);
-     console.log(iares)
-    }).catch((error) =>{
-      console.log(error);
-    });
+        <div className='mt-3'>
+          {messages.map((message, index) => (
+            <p key={index}>
+              {message.sender}: {message.text}
+            </p>
+          ))}
+        </div>
+      </Container>
+    </>
+  );
 }
 
-return (
-<>
-<Container className='mt-5'>
-<h1> Respuesta IA</h1>
-
-<Form.Group className='mb-3'>
-    <Form.Control
-      type='text'
-      name='prompt'
-      placeholder='ingresa pregunta'
-      onChange={(e) => SetPrompt(e.target.value)}
-    />
-  </Form.Group>
-      <Button onClick={handleclick}>Preguntar</Button>
-
-      <p className="mt-3">{iares.message}</p>
-     
-  </Container>
-</>
-
-);
-}
-
-
-
-export default OpenIA ;
+export default OpenIA;
